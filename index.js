7b1027cf30be00a09c532457ff53e777f79e807b
Kyle E. Mitchell <kyle@kemitchell.com>
2022-06-01 21:14:49 -0700

    Replace read-package-tree with Arborist

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 module.exports = licensee
 
 var blueOakList = require('@blueoak/list')
+var Arborist = require('@npmcli/arborist')
 var correctLicenseMetadata = require('correct-license-metadata')
 var has = require('has')
 var npmLicenseCorrections = require('npm-license-corrections')
 var osi = require('spdx-osi')
 var parse = require('spdx-expression-parse')
-var parseJSON = require('json-parse-errback')
-var readPackageTree = require('read-package-tree')
-var runParallel = require('run-parallel')
 var satisfies = require('semver').satisfies
-var simpleConcat = require('simple-concat')
-var spawn = require('child_process').spawn
 var spdxAllowed = require('spdx-whitelisted')
 
 function licensee (configuration, path, callback) {
@@ -37,111 +33,29 @@ function licensee (configuration, path, callback) {
   ) {
     callback(new Error('No licenses or packages allowed.'))
   } else {
-    if (configuration.productionOnly) {
-      // In order to ignore devDependencies, we need to read:
-      //
-      // 1. the dependencies-only dependency graph, from
-      //    `npm ls --json --production`
-      //
-      // 2. the structure of `node_modules` and `package.json`
-      //    files within it, with read-package-tree.
-      //
-      // `npm ls` calls read-package-tree internally, but does
-      // lots of npm-specific post-processing to produce the
-      // dependency tree.  Calling read-package-tree twice, at
-      // the same time, is far from efficient.  But it works,
-      // and doing so helps keep this package small.
-      runParallel({
-        dependencies: readDependencyList,
-        packages: readFilesystemTree
-      }, function (error, trees) {
-        if (error) callback(error)
-        else withTrees(trees.packages, trees.dependencies)
-      })
-    } else {
-      // If we are analyzing _all_ installed dependencies,
-      // and don't care whether they're devDependencies
-      // or not, just read `node_modules`.  We don't need
-      // the dependency graph.
-      readFilesystemTree(function (error, packages) {
-        if (error) callback(error)
-        else {
-          if (configuration.filterPackages) {
-            packages = configuration.filterPackages(packages)
-          }
-          withTrees(packages, false)
+    readFilesystemTree(function (error, children) {
+      if (error) callback(error)
+      else {
+        if (configuration.filterPackages) {
+          children = configuration.filterPackages(children)
         }
-      })
-    }
-  }
-
-  function withTrees (packages, dependencies) {
-    callback(null, findIssues(
-      configuration, packages, dependencies, []
-    ))
-  }
-
-  function readDependencyList (done) {
-    var executable = process.platform === 'win32' ? 'npm.cmd' : 'npm'
-    var child = spawn(
-      executable, ['ls', '--production', '--json'], { cwd: path }
-    )
-    var outputError
-    var json
-    simpleConcat(child.stdout, function (error, buffer) {
-      if (error) outputError = error
-      else json = buffer
-    })
-    child.once('error', function (error) {
-      outputError = error
-    })
-    child.once('close', function (code) {
-      if (outputError) {
-        done(outputError)
-      } else {
-        if (code !== 0) console.error('Warning: npm exited with status ' + code)
-
-        parseJSON(json, function (error, graph) {
-          if (error) return done(error)
-          if (!has(graph, 'dependencies')) {
-            done(new Error('cannot interpret npm ls --json output'))
-          } else {
-            var flattened = {}
-            flattenDependencyTree(graph.dependencies, flattened)
-            done(null, flattened)
-          }
-        })
+        callback(null, findIssues(
+          configuration, children, []
+        ))
       }
     })
   }
 
   function readFilesystemTree (done) {
-    readPackageTree(path, function (error, tree) {
-      if (error) return done(error)
-      done(null, tree.children)
-    })
+    var arborist = new Arborist({ path })
+    arborist.loadActual()
+      .catch(function (error) {
+        return done(error)
+      })
+      .then(function (tree) {
+        return done(null, tree.children)
+      })
   }
-}
-
-var KEY_PREFIX = '.'
-
-function flattenDependencyTree (graph, object) {
-  Object.keys(graph).forEach(function (name) {
-    var node = graph[name]
-    var version = node.version
-    var key = KEY_PREFIX + name
-    if (
-      has(object, key) &&
-      object[key].indexOf(version) === -1
-    ) {
-      object[key].push(version)
-    } else {
-      object[key] = [version]
-    }
-    if (has(node, 'dependencies')) {
-      flattenDependencyTree(node.dependencies, object)
-    }
-  })
 }
 
 function validConfiguration (configuration) {
@@ -169,12 +83,12 @@ function isString (argument) {
   return typeof argument === 'string'
 }
 
-function findIssues (configuration, children, dependencies, results) {
-  if (Array.isArray(children)) {
-    children.forEach(function (child) {
+function findIssues (configuration, children, results) {
+  if (children) {
+    Array.from(children.values()).forEach(function (child) {
       if (
         !configuration.productionOnly ||
-        appearsIn(child, dependencies)
+        !child.dev
       ) {
         var result = resultForPackage(configuration, child)
         // Deduplicate.
@@ -193,38 +107,24 @@ function findIssues (configuration, children, dependencies, results) {
         } else {
           results.push(result)
         }
-        findIssues(configuration, child, dependencies, results)
+        findIssues(configuration, child, results)
       }
       if (child.children) {
-        findIssues(configuration, child.children, dependencies, results)
+        findIssues(configuration, child.children, results)
       }
     })
     return results
   } else return results
 }
 
-function appearsIn (installed, dependencies) {
-  var name = installed.package.name
-  var key = KEY_PREFIX + name
-  var version = installed.package.version
-  return (
-    has(dependencies, key) &&
-    dependencies[key].indexOf(version) !== -1
-  )
-}
-
 function resultForPackage (configuration, tree) {
   var packageAllowlist = configuration.packages || {}
+  // TODO: fetch license with path
   var result = {
-    name: tree.package.name,
-    license: tree.package.license,
-    author: tree.package.author,
-    contributors: tree.package.contributors,
-    repository: tree.package.repository,
-    homepage: tree.package.homepage,
-    version: tree.package.version,
+    name: tree.name,
+    version: tree.version,
     parent: tree.parent,
-    path: tree.path
+    path: tree.realpath
   }
 
   // Find and apply any manual license metadata correction.
