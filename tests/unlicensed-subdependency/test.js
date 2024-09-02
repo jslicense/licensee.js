3686941761029b9afb896bc45137cc59579197d9
Kyle E. Mitchell <kyle@kemitchell.com>
2024-09-01 21:46:01 -0700

    Replace `const` with old `var` for consistency

--- tests/unlicensed-subdependency/test.js
+++ tests/unlicensed-subdependency/test.js
@@ -4,7 +4,7 @@ var results = require('../run')(['--ndjson'], __dirname)
 
 tap.equal(results.status, 1)
 
-const output = results.stdout.trim().split('\n').map(line => JSON.parse(line))
+var output = results.stdout.trim().split('\n').map(line => JSON.parse(line))
 
 tap.assert(
   output.some(result => result.name === 'mit-licensed-depends-on-not-licensed' && result.approved === false)
